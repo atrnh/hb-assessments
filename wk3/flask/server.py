940d54b91383dccdaee85d94dae7400001669cfc
ashley.triinh@gmail.com <atrnh@teknik.io>
2017-03-10 22:06:54 -0800

    Added application-success page

--- wk3/flask/server.py
+++ wk3/flask/server.py
@@ -11,13 +11,35 @@ app.secret_key = "ABC"
 
 @app.route("/")
 def index():
+    """Returns the homepage."""
+
     return render_template("index.html")
 
 
 @app.route("/application-form")
 def application():
-    return render_template("application-response.html")
+    """Returns application with a list of available jobs."""
 
+    return render_template("application-response.html",
+                           jobs=["Software Engineer",
+                                 "QA Engineer",
+                                 "Product Manager"])
+
+
+@app.route("/application-success")
+def application_success():
+    """Displays info submitted from job application."""
+
+    first_name = request.args.get("first_name")
+    last_name = request.args.get("last_name")
+    salary = float(request.args.get("salary"))
+    job = request.args.get("job")
+
+    return render_template("application-success.html",
+                           first_name=first_name,
+                           last_name=last_name,
+                           salary=salary,
+                           job=job)
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
